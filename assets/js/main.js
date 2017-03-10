f3de16d2c89eea0a69bb9f4bcd49c1acf87fe589
omnikrll <kfhohn@gmail.com>
2017-03-09 21:38:42 -0500

    added polygon areas over girl sprite with hover events

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,77 @@ var Main = function(game) {
 	girl = null;
 	drone = null;
 	smoke = null;
+
+	backpackClickZone = null;
+	phoneClickZone = null;
+
+	backpackToolTip = null;
+	phoneToolTip = null;
 };
 
 Main.prototype = {
 	create: function() {
+		// add images
 		background = this.game.add.sprite(0, 0, 'background');
-		// background.scale.setTo(900, 600);
 		girl = this.game.add.sprite(120, this.game.world.height - 400, 'girl');		
 		drone = this.game.add.sprite(this.game.world.width - 286, 0, 'drone');
 		smoke = this.game.add.sprite(this.game.world.width - 320, (this.game.world.height / 2) - 180, 'smoke');
+
+		this.addBackpack();
+		this.addPhone();
 	},
-	
+
+	update: function() {
+		if (backpackClickZone.input.pointerOver()) {
+			backpackToolTip.x = this.game.input.x - 18;
+			backpackToolTip.y = this.game.input.y - 36;
+		} else {
+			backpackToolTip.x = -1000;
+			backpackToolTip.y = -1000;
+		}
+		if (phoneClickZone.input.pointerOver()) {
+			phoneToolTip.x = this.game.input.x - 18;
+			phoneToolTip.y = this.game.input.y - 36;
+		} else {
+			phoneToolTip.x = -1000;
+			phoneToolTip.y = -1000;
+		}
+	},
+
+	addBackpack: function() {
+		//add geometry for clickable areas for backpack / phone
+		backpackClickZone = this.game.add.graphics(178, this.game.world.height - 248);
+		backpackClickZone.beginFill(0xFFFFFF, 0);
+
+		backpackClickZone.moveTo(0, 0);
+		backpackClickZone.lineTo(0, 44);
+		backpackClickZone.lineTo(68, 20);
+		backpackClickZone.lineTo(110, 30);
+		backpackClickZone.lineTo(128, 60);
+		backpackClickZone.lineTo(128, 0);
+		backpackClickZone.lineTo(68, 10);
+		backpackClickZone.lineTo(0, 0);
+		backpackClickZone.endFill();
+
+		backpackClickZone.inputEnabled = true;
+
+		backpackToolTip = this.game.add.text(-1000, -1000, 'open backpack', {font: '18px Arial', fill: '#ffffff'});
+	},
+
+	addPhone: function() {
+
+		phoneClickZone = this.game.add.graphics(294, this.game.world.height - 64);
+		phoneClickZone.beginFill(0xFFFFFF, 0);
+
+		phoneClickZone.moveTo(4, 0);
+		phoneClickZone.lineTo(0, 16);
+		phoneClickZone.lineTo(42, 28);
+		phoneClickZone.lineTo(48, 12);
+		phoneClickZone.lineTo(4, 0);
+		phoneClickZone.endFill();
+
+		phoneClickZone.inputEnabled = true;
+
+		phoneToolTip = this.game.add.text(-1000, -1000, 'open phone', {font: '18px Arial', fill: '#ffffff'});
+	}
 };
