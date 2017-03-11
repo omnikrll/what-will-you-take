546189c8323ac20a8ef965d020450c067c8111cb
omnikrll <kfhohn@gmail.com>
2017-03-10 23:43:42 -0500

    dummy interactions for phone and backpack

--- assets/js/backpack.js
+++ assets/js/backpack.js
@@ -1,12 +1,16 @@
 var Backpack = function(game) {
-
+	placeholderText = null;
 };
 
 Backpack.prototype = {
-	preload: function() {
-
-	},
 	create: function() {
-		
+		placeholderText = this.game.add.text(100, 100, 'Back', { font: '24px Arial', fill: '#ffffff'});
+		placeholderText.inputEnabled = true;
+
+		var back = function() {
+			this.game.state.start('Main');
+		};
+
+		placeholderText.events.onInputDown.add(back, this);
 	}
 };
